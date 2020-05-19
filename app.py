2ec4acfcacffe8db2ebb717f69c59a18b1c10098
mdhwang <mattdhwang@gmail.com>
2020-05-19 02:06:32 -0700

    added logic for all 5 attachemnts

--- app.py
+++ app.py
@@ -17,42 +17,160 @@ df = pd.read_csv('attachment_data.csv',dtype=formatting)
 
 weapons = create_options(df.Weapon.unique())
 
-stocks_mask = (df.Weapon == "M4A1") & (df.Category == "STOCK")
-stocks = create_options(df[stocks_mask].Attachment.to_list())
+empty=[]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
+
+    html.H2('CHOOSE YOUR WEAPON'),
     dcc.Dropdown(
         id='weapon-dropdown',
         options=weapons,
-        value='M4A1'
+        value='None'
     ),
-    html.Div(id='dd-output-container'),
     html.Br(),
+
+    html.H5('Attachment 1'),
+    html.P('Category:'),
     dcc.Dropdown(
-        id='stocks',
-        options=stocks,
-        value='Choose Stock Attachment'
+        id='cat1',
+        options=empty,
+        value='None'
     ),
-    html.Div(id='stock-output-container'),
+    html.Br(),
+    html.P('Attachment:'),
+    dcc.Dropdown(
+        id='att1',
+        options=empty,
+        value='None'
+    ),
+
+    html.Br(),
+    html.H5('Attachment 2'),
+    html.P('Category:'),
+    dcc.Dropdown(
+        id='cat2',
+        options=empty,
+        value='None'
+    ),
+    html.Br(),
+    html.P('Attachment:'),
+    dcc.Dropdown(
+        id='att2',
+        options=empty,
+        value='None'
+    ),
+
+    html.Br(),
+    html.H5('Attachment 3'),
+    html.P('Category:'),
+    dcc.Dropdown(
+        id='cat3',
+        options=empty,
+        value='None'
+    ),
+    html.Br(),
+    html.P('Attachment:'),
+    dcc.Dropdown(
+        id='att3',
+        options=empty,
+        value='None'
+    ),
+
+    html.Br(),
+    html.H5('Attachment 4'),
+    html.P('Category:'),
+    dcc.Dropdown(
+        id='cat4',
+        options=empty,
+        value='None'
+    ),
+    html.Br(),
+    html.P('Attachment:'),
+    dcc.Dropdown(
+        id='att4',
+        options=empty,
+        value='None'
+    ),
+
+    html.Br(),
+    html.H5('Attachment 5'),
+    html.P('Category:'),
+    dcc.Dropdown(
+        id='cat5',
+        options=empty,
+        value='None'
+    ),
+    html.Br(),
+    html.P('Attachment:'),
+    dcc.Dropdown(
+        id='att5',
+        options=empty,
+        value='None'
+    ),
+
+
+    
 ])
 
 
-@app.callback(
-    dash.dependencies.Output('dd-output-container', 'children'),
-    [dash.dependencies.Input('weapon-dropdown', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
-
 @app.callback([
-    dash.dependencies.Output('stocks', 'children'),
-    dash.dependencies.Output('stocks', 'options'),
-    dash.dependencies.Output('stocks', 'value')],
+    dash.dependencies.Output('cat1', 'options'),
+    dash.dependencies.Output('cat2', 'options'),
+    dash.dependencies.Output('cat3', 'options'),
+    dash.dependencies.Output('cat4', 'options'),
+    dash.dependencies.Output('cat5', 'options'),],
     [dash.dependencies.Input('weapon-dropdown', 'value')])
 def update_dropdown(value):
-    stocks_mask = (df.Weapon == value) & (df.Category == "STOCK")
-    stocks = create_options(df[stocks_mask].Attachment.to_list())
-    return value, stocks, value
+    cat_mask = (df.Weapon == value)
+    cat = create_options(df[cat_mask].Category.unique())
+    return cat, cat, cat, cat, cat
+
+@app.callback(
+    dash.dependencies.Output('att1', 'options'),
+    [dash.dependencies.Input('weapon-dropdown', 'value'),
+    dash.dependencies.Input('cat1', 'value'),])
+def update_dropdown(wep_val,cat_val):
+    att_mask = (df.Weapon == wep_val) & (df.Category == cat_val)
+    att = create_options(df[att_mask].Attachment.to_list())
+    return att
+
+@app.callback(
+    dash.dependencies.Output('att2', 'options'),
+    [dash.dependencies.Input('weapon-dropdown', 'value'),
+    dash.dependencies.Input('cat2', 'value'),])
+def update_dropdown(wep_val,cat_val):
+    att_mask = (df.Weapon == wep_val) & (df.Category == cat_val)
+    att = create_options(df[att_mask].Attachment.to_list())
+    return att
+
+@app.callback(
+    dash.dependencies.Output('att3', 'options'),
+    [dash.dependencies.Input('weapon-dropdown', 'value'),
+    dash.dependencies.Input('cat3', 'value'),])
+def update_dropdown(wep_val,cat_val):
+    att_mask = (df.Weapon == wep_val) & (df.Category == cat_val)
+    att = create_options(df[att_mask].Attachment.to_list())
+    return att
+
+@app.callback(
+    dash.dependencies.Output('att4', 'options'),
+    [dash.dependencies.Input('weapon-dropdown', 'value'),
+    dash.dependencies.Input('cat4', 'value'),])
+def update_dropdown(wep_val,cat_val):
+    att_mask = (df.Weapon == wep_val) & (df.Category == cat_val)
+    att = create_options(df[att_mask].Attachment.to_list())
+    return att
+
+@app.callback(
+    dash.dependencies.Output('att5', 'options'),
+    [dash.dependencies.Input('weapon-dropdown', 'value'),
+    dash.dependencies.Input('cat5', 'value'),])
+def update_dropdown(wep_val,cat_val):
+    att_mask = (df.Weapon == wep_val) & (df.Category == cat_val)
+    att = create_options(df[att_mask].Attachment.to_list())
+    return att
+
 
 
 
