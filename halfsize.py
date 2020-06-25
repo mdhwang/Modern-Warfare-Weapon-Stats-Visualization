74da768f7a1147793d9da8a0eba8aa4e2a76c589
mdhwang <mattdhwang@gmail.com>
2020-06-25 12:39:37 -0700

    updated parse script with BW images

---
+++ halfsize.py
@@ -0,0 +1,36 @@
+import cv2
+
+from os import listdir
+
+from image_helpers import *
+
+
+q = input('Will overwrite screenshots from Guns folder with cropped and BW versions - Continue? y/n? : ')
+if q == "y":
+    print("Running Program")
+    count = 0
+    classes = listdir('data/Guns/')
+    if '.DS_Store' in classes:
+        classes.remove(".DS_Store")
+    for wep_class in classes:
+        guns = listdir('data/Guns/{}'.format(wep_class))
+        if '.DS_Store' in guns:
+            guns.remove(".DS_Store")
+        for gun in guns:
+            categories = listdir('data/Guns/{}/{}'.format(wep_class, gun))
+            if '.DS_Store' in categories:
+                categories.remove(".DS_Store")
+            for category in categories:
+                attachments = listdir('data/Guns/{}/{}/{}'.format(wep_class, gun, category))
+                if '.DS_Store' in attachments:
+                    attachments.remove('.DS_Store')
+                for attachment in attachments:
+                    path = 'data/Guns/{}/{}/{}/{}'.format(wep_class, gun, category, attachment)
+                    img = cv2.imread(path, 0)
+                    cropped = halfsize(img)
+                    cv2.imwrite(path,cropped)
+                    print("Processed {} images".format(count))
+                    count += 1
+    print("Completed")
+else:
+    print("Skipped")
--- image_helpers.py
+++ image_helpers.py
@@ -17,9 +17,6 @@ def halfsize(img):
     y,x = img.shape
     half = int(x/2)
     cropped = img[0:y,0:half]
-    # cv2.imshow('test',cropped)
-    # cv2.waitKey(0)  
-    # cv2.destroyAllWindows() 
     return cropped
 
 def get_stats(img):
@@ -44,33 +41,3 @@ def get_stats(img):
     rows.append(row)
     return attachment_name, attachment_stats
 
-def muzzle(img):
-    pass
-
-def barrel(img):
-    pass
-
-def laser(img):
-    pass
-
-def optic(img):
-    pass
-
-def stock(img):
-    pass
-
-def perk(img):
-    pass
-
-def grip(img):
-    pass
-
-def ammunition(img):
-    pass
-
-def underbarrel(img):
-    pass
-
-# img = cv2.imread('aug.png',0)
-# cropped = halfsize(img)
-# cv2.imwrite('aug.png',cropped)
